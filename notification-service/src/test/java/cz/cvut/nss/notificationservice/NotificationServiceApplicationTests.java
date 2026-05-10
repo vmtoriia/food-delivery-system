96c5c29b8348d031e18656c14a70d3f5fd38b2b5
Oleksandr Bobrov - Gitlab <bobroole@fel.cvut.cz>
2026-05-10 13:01:53 +0200

    spring boot init

---
+++ notification-service/src/test/java/cz/cvut/nss/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.cvut.nss.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
